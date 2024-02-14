1aa9b10570ae328763ec5fbdb0f4ef7846a9143d
kchiwakaya <kchiwakaya@gmail.com>
2024-02-14 16:48:35 +0200

    cool

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,9 +10,9 @@ def number_of_subscribers(subreddit):
     headers = {'user-agent': 'egsyquest'}
     r = requests.get(url, headers=headers)
 
-    if (r.status_code == 302):
+    if (r.status_code is 302):
         return 0
-    if (r.status_code == 404):
+    if (r.status_code is 404):
         return 0
 
     return r.json()['data'].get('subscribers', 0)
