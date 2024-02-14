4b5f31c62788b4e8d253b611de3a29d8e8e96532
kchiwakaya <kchiwakaya@gmail.com>
2024-02-14 17:02:58 +0200

    decision tree

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# puppet script that applies fix to Apache server returning a 500 error
+# script that fix an Apache server returning error 500
 exec { 'fix-apache':
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => ['/usr/bin', '/usr/sbin', '/bin']
