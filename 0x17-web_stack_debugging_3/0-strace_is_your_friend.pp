a10d6893d2024a8493f0a54a63d59cedc8837ca6
Kevin Chiwakaya <40562667+kchiwakaya@users.noreply.github.com>
2024-04-08 08:20:46 +0200

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# script that fix an Apache server returning error 500
-exec { 'fix-apache':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => ['/usr/bin', '/usr/sbin', '/bin']
-  }
-
